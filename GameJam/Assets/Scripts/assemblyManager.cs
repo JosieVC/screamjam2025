1015bd92aef33500cb1e7cc461d8ba59398104d1
Yen-Zhu <yeyanzhu10@gmail.com>
2025-10-16 19:32:12 -0400

    Updated Assembly, player hand now follows mouse, with the mouse between the fingers

--- GameJam/Assets/Scripts/assemblyManager.cs
+++ GameJam/Assets/Scripts/assemblyManager.cs
@@ -1,13 +1,26 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class assemblyManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject boxPrefab;
+
+    private List<GameObject> boxes;
+    private List<Vector3> boxPositions;
     public static int money;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Adds a list of boxes.
+        boxes = new List<GameObject>();
+        for (int i = 0; i < 5; i++)
+        {
+            boxes.Add(Instantiate(boxPrefab));
+            boxes[i].GetComponent<box>().AssignBox(i);
+        }
+        AddPositions();
     }
 
     // Update is called once per frame
@@ -16,4 +29,21 @@ public class assemblyManager : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Add the positions to each box.
+    /// </summary>
+    private void AddPositions()
+    {
+        boxPositions = new List<Vector3>();
+        boxPositions.Add(new Vector3(-6.49f, -1.34f, 9f));
+        boxPositions.Add(new Vector3(-7.07f, -2.37f, 8f));
+        boxPositions.Add(new Vector3(-7.91f, -3.87f, 7f));
+        boxPositions.Add(new Vector3(-2.91f, -2.01f, 6f));
+        boxPositions.Add(new Vector3(-3.31f, -3.28f, 5f));
+
+        for (int i = 0; i < 5; i++)
+        {
+            boxes[i].transform.position = boxPositions[i];
+        }
+    }
 }
--- GameJam/Assets/Scripts/player.cs
+++ GameJam/Assets/Scripts/player.cs
@@ -1,22 +1,65 @@
 using UnityEngine;
-using static UnityEngine.UI.Image;
+using UnityEngine.InputSystem;
 
 public class player : MonoBehaviour
 {
-    [SerializeField]
-    private Sprite relaxed;
-    [SerializeField]
-    private Sprite grab;
+    [SerializeField] private Sprite relaxed;
+    [SerializeField] private Sprite grab;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private SpriteRenderer sr;
+    private float maxY = -1f;
+
+    private bool isGrabbing = false;
+
+    // Offset so mouse is top-left of the hand
+    private Vector3 handOffset = new Vector3(2.8f, -1f, 0f);
+
+    // Additional small grab offset
+    private Vector3 grabOffset = new Vector3(0.16f, -0.4f, 0f);
+
     void Start()
     {
-        GetComponent<SpriteRenderer>().sprite = relaxed;
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = relaxed;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get mouse position in world space
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        worldPos.z = 0f;
+
+        // Clamp Y axis
+        if (worldPos.y >= maxY) 
+        { 
+            worldPos.y = maxY; 
+        }
+
+        // Start with mouse + hand offset
+        Vector3 finalPos = worldPos + handOffset;
+
+        // Apply grabbing offset if grabbing
+        if (isGrabbing)
+            finalPos += grabOffset;
+
+        transform.position = finalPos;
+    }
+
+    /// <summary>
+    /// When attack button is pressed, the hand grabs.
+    /// </summary>
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            sr.sprite = grab;
+            isGrabbing = true;
+        }
+        else if (context.canceled)
+        {
+            sr.sprite = relaxed;
+            isGrabbing = false;
+        }
     }
 }
