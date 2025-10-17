15bcfb6b6eaa16969cc349026f8e741a3ef6ca0a
Yen-Zhu <yeyanzhu10@gmail.com>
2025-10-16 21:30:00 -0400

    Assembly interaction update

--- GameJam/Assets/Scripts/blueprint.cs
+++ GameJam/Assets/Scripts/blueprint.cs
@@ -1,7 +1,32 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class bluepriint : MonoBehaviour
+public class blueprint : MonoBehaviour
 {
+    [SerializeField]
+    private Sprite brainBPPrefab;
+    [SerializeField]
+    private Sprite stomachBPPrefab;
+    [SerializeField]
+    private Sprite lungsBPPrefab;
+    [SerializeField]
+    private Sprite skinBPPrefab;
+    [SerializeField]
+    private Sprite heartBPPrefab;
+
+    [SerializeField]
+    private GameObject brain;
+    [SerializeField]
+    private GameObject stomach;
+    [SerializeField]
+    private GameObject lungs;
+    [SerializeField]
+    private GameObject skin;
+    [SerializeField]
+    private GameObject heart;
+
+    private GameObject organ;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,7 +41,38 @@ public class bluepriint : MonoBehaviour
 
     void MakeOrgan()
     {
-        //this is the bulk of this scene - check to see if the player has enough parts for the recipe, if so, place them on the blueprint,
-        //assemble the object and award the player money (money is a static int, access it with assemblyManager.money)
+
+    }
+
+    /// <summary>
+    /// Sets which blueprint it displays.
+    /// </summary>
+    /// 
+    /// <param name="num"></param>
+    public void SetBlueprint(int num)
+    {
+        switch (num)
+        {
+            case 0:
+                GetComponent<SpriteRenderer>().sprite = brainBPPrefab;
+                //organ = brain;
+                break;
+            case 1:
+                GetComponent<SpriteRenderer>().sprite = stomachBPPrefab;
+                //organ = stomach;
+                break;
+            case 2:
+                GetComponent<SpriteRenderer>().sprite = lungsBPPrefab;
+                //organ = lungs;
+                break;
+            case 3:
+                GetComponent<SpriteRenderer>().sprite = skinBPPrefab;
+                //organ = skin;
+                break;
+            case 4:
+                GetComponent<SpriteRenderer>().sprite = heartBPPrefab;
+                //organ = heart;
+                break;
+        }
     }
 }
